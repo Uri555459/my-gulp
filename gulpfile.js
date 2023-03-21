ab863c193a5a8da3a9fa59b66813f5d21b1d5332
Uri <uri555459@gmail.com>
2023-03-21 11:46:52 +0300

    Added support scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const { src, dest } = require('gulp')
 const scss = require('gulp-sass')(require('sass'))
 const concat = require('gulp-concat')
+const uglify = require('gulp-uglify-es').default
 
 function styles() {
 	return src('app/scss/style.scss')
@@ -9,4 +10,12 @@ function styles() {
 		.pipe(dest('app/css'))
 }
 
+function scripts() {
+	return src('app/js/main.js')
+		.pipe(concat('main.min.js'))
+		.pipe(uglify())
+		.pipe(dest('app/js'))
+}
+
 exports.styles = styles
+exports.scripts = scripts
